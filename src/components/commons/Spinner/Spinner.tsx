763f2aecad097796f48f6541e0b6d3af439dc041
Mateusz <nowe7246@gmail.com>
2022-09-27 16:31:46 +0200

    Create spinner component.

---
+++ src/components/commons/Spinner/Spinner.tsx
@@ -0,0 +1,8 @@
+import './spinner.css';
+
+export const Spinner = () => (
+    <div className="center">
+        <div className='spinner'/>
+        <span>Wczytywanie danych...</span>
+    </div>
+)
