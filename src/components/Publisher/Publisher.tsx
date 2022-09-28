e6c07d2e5ffde6feb088afa8351c0fd18487df13
Mateusz <nowe7246@gmail.com>
2022-09-28 14:37:23 +0200

    Created simple view of publisher component.

---
+++ src/components/Publisher/Publisher.tsx
@@ -0,0 +1,15 @@
+import './Publisher.css';
+
+export const Publisher = () => {
+    return (
+        <div className='publisher__container'>
+            <h2>Nazwa wydawcy</h2>
+            <p>opis</p>
+            <div className="studios__container">
+                <div className="studio">
+                    <p>Nazwa</p>
+                </div>
+            </div>
+        </div>
+    )
+}
