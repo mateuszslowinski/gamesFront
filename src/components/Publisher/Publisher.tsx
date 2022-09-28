057fd00f2d595b044ff5677b37e8525fc7c4d534
Mateusz <nowe7246@gmail.com>
2022-09-28 15:28:19 +0200

    Gets publisher data from backend.

--- src/components/Publisher/Publisher.tsx
+++ src/components/Publisher/Publisher.tsx
@@ -1,15 +1,37 @@
+import {useParams} from "react-router-dom";
+import {useApi} from "../../hooks/useApi";
+import {Spinner} from "../commons/Spinner/Spinner";
+import {ErrorMessage} from "../commons/ErrorMessage/ErrorMessage";
+import {StudioType} from 'types';
+
 import './Publisher.css';
 
 export const Publisher = () => {
+    const {id} = useParams();
+
+    const [response, loading, error] = useApi<StudioType[]>({
+        method: 'get',
+        url: `/publisher/${id}/studios`
+    }, id);
+
+    if(error) return <ErrorMessage text={error}/>
     return (
-        <div className='publisher__container'>
-            <h2>Nazwa wydawcy</h2>
-            <p>opis</p>
-            <div className="studios__container">
-                <div className="studio">
-                    <p>Nazwa</p>
-                </div>
-            </div>
-        </div>
+        <>
+            {
+                loading ? <Spinner/> : (
+                    <div className='publisher__container'>
+                        <h2>Nazwa wydawcy</h2>
+                        <p>opis</p>
+                        <div className="studios__container">
+                            {response?.map(studio => (
+                                <div className="studio" key={studio.id}>
+                                    <p>{studio.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
