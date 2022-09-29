6a5bd9063e5eba71dd2ae190c251629b3d6a7de9
Mateusz <nowe7246@gmail.com>
2022-09-29 16:38:28 +0200

    Connected route studio page to app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import { Header } from './components/commons/Header/Header';
+import {Header} from './components/commons/Header/Header';
 import {HomePage} from "./pages/Home/Home.page";
 import {PublisherPage} from "./pages/Publisher/Publisher.page";
+import {StudioPage} from "./pages/Studio/Studio.page";
 
 export const App = () => {
     return (
@@ -11,6 +12,7 @@ export const App = () => {
             <Routes>
                 <Route path='/' element={<HomePage/>}/>
                 <Route path='/publisher/:id' element={<PublisherPage/>}/>
+                <Route path='/studio/:id' element={<StudioPage/>}/>
             </Routes>
         </>
     );
