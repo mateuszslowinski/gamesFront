82e7d99b2e4eae24d9915814f45cec0378ec7f08
Mateusz <nowe7246@gmail.com>
2022-09-30 10:24:45 +0200

    Added route for login compenent.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from './components/commons/Header/Header';
 import {HomePage} from "./pages/Home/Home.page";
 import {PublisherPage} from "./pages/Publisher/Publisher.page";
 import {StudioPage} from "./pages/Studio/Studio.page";
+import {Login} from "./components/Login/Login";
 
 export const App = () => {
     return (
@@ -11,6 +12,7 @@ export const App = () => {
             <Header/>
             <Routes>
                 <Route path='/' element={<HomePage/>}/>
+                <Route path='/auth/login' element={<Login/>}/>
                 <Route path='/publisher/:id' element={<PublisherPage/>}/>
                 <Route path='/studio/:id' element={<StudioPage/>}/>
             </Routes>
