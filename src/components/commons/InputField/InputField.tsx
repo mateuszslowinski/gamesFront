d188d1246ba34f6606f459b21eb2478f36abf64a
Mateusz <nowe7246@gmail.com>
2022-10-04 10:14:59 +0200

    Adjusted input component to react-hook-form.

--- src/components/commons/InputField/InputField.tsx
+++ src/components/commons/InputField/InputField.tsx
@@ -1,20 +1,26 @@
 import React, {ChangeEvent} from "react";
+import {UseFormRegisterReturn} from "react-hook-form";
 import './InputFiled.css';
 
 interface Props {
     type: React.HTMLInputTypeAttribute,
-    name: string
     value: string
     placeholder: string
     onChange: (e: ChangeEvent<HTMLInputElement>) => void
+    error: any
+    validation?: UseFormRegisterReturn
 }
 
-export const InputField = ({type, name, value, placeholder, onChange}: Props) => (
-    <input
-        type={type}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-    />
+export const InputField = ({ type, value, placeholder, onChange, error, validation}: Props) => (
+    <>
+        <input
+            type={type}
+            value={value}
+            {...validation}
+            placeholder={placeholder}
+            onChange={onChange}
+        />
+        {error && <div className='input__error__message'>{error.message}</div>}
+    </>
+
 )
