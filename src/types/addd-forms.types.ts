4a58d55e5820337a1397baa3d5df67c4df6536e7
Mateusz <nowe7246@gmail.com>
2022-10-04 16:06:40 +0200

    Created add studio form type.

--- src/types/addd-forms.types.ts
+++ src/types/addd-forms.types.ts
@@ -4,3 +4,11 @@ export type AddPublisherFormType = {
 }
 
 export type AddPlatformFormType = AddPublisherFormType;
+
+export interface AddStudioFormType extends AddPublisherFormType {
+    ownerId: string
+    country: string
+    founded: string
+    employees: number
+    image: File | null
+}
