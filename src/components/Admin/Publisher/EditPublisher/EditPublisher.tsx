236484ef2824691399660fc8987fff0cc529c019
Mateusz <nowe7246@gmail.com>
2022-10-10 17:24:12 +0200

    Created basic structure of edit publisher form.

---
+++ src/components/Admin/Publisher/EditPublisher/EditPublisher.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {useForm} from "react-hook-form";
+import {Form} from "../../Form";
+import {InputField} from "../../../commons/FormFields/InputField/InputField";
+import {TextAreaField} from "../../../commons/FormFields/TextArea/TextAreaField";
+import {EditPublisherFormType} from "../../../../types/edit-forms.types";
+
+interface Props{
+    closeModal:(value: number) => void
+}
+
+export const EditPublisher =({closeModal}:Props)=>{
+
+    const {
+        register,
+        formState: {
+            errors: {name, description},
+        },
+    } = useForm<EditPublisherFormType>({
+        defaultValues: {
+            name: '',
+            description: ""
+        }
+    });
+
+
+    return(
+        <Form
+            onSubmit={()=>{}}
+            closeModal={()=>{}}
+            formSubtitle='Edytuj nowego wydawce:'
+            error=''
+            buttonTxt='Edytuj'
+        >
+            <InputField
+                type='text'
+                placeholder='Nazwa wydawcy...'
+                error={name}
+                validation={register('name', {
+                    required: 'Nazwa wydawcy jest wymagana',
+                    maxLength: {
+                        value: 50,
+                        message: 'Nazwa nie może przekraczać 50 znaków',
+                    },
+                })}
+                text='Nazwa wydawcy'
+            />
+            <TextAreaField
+                error={description}
+                validation={register('description', {
+                    required: 'Opis wydawcy jest wymagany',
+                    maxLength: {
+                        value: 1500,
+                        message: 'Opis wydawcy nie może przekraczać 1500 znaków',
+                    },
+                })}
+                placeholder='Opis wydawcy...'
+            />
+        </Form>
+    )
+}
