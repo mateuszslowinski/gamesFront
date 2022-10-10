29b3f4e4ead87fe433d36b73f0f9c4f7c03e14f1
Mateusz <nowe7246@gmail.com>
2022-10-10 18:42:07 +0200

    Added edit publisher form on submit method.

--- src/components/Admin/Publisher/EditPublisher/EditPublisher.tsx
+++ src/components/Admin/Publisher/EditPublisher/EditPublisher.tsx
@@ -1,4 +1,7 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 import {useForm} from "react-hook-form";
+import {api} from "../../../../utils/axios";
 import {Form} from "../../Form";
 import {InputField} from "../../../commons/FormFields/InputField/InputField";
 import {TextAreaField} from "../../../commons/FormFields/TextArea/TextAreaField";
@@ -8,24 +11,45 @@ import {PublisherType} from 'types';
 interface Props {
     closeModal: (value: number) => void
     publisher: PublisherType
+    token: string | null
 }
 
-export const EditPublisher = ({closeModal, publisher}: Props) => {
+export const EditPublisher = ({closeModal, publisher, token}: Props) => {
+    const navigate = useNavigate();
+    const [error, setError] = useState('');
     const {
+        handleSubmit,
         register,
         formState: {
             errors: {name, description},
         },
     } = useForm<EditPublisherFormType>();
 
+
+    const onSubmit = async (data: EditPublisherFormType) => {
+        try {
+            const response = await api.patch(`/publisher/${publisher.id}`, data, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            if (response.status === 200) {
+                navigate(0)
+            } else {
+                setError(response.data.error);
+            }
+
+        } catch (e) {
+            setError((e as Error).message);
+        }
+    }
+
     return (
         <Form
-            onSubmit={() => {
-            }}
-            closeModal={() => {
-            }}
+            onSubmit={handleSubmit(onSubmit)}
+            closeModal={closeModal}
             formSubtitle='Edytuj nowego wydawce:'
-            error=''
+            error={error}
             buttonTxt='Edytuj'
         >
             <InputField
