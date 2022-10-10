43524feec62ad7059d09a8c413eedfd933f4e904
Mateusz <nowe7246@gmail.com>
2022-10-10 15:17:12 +0200

    Added button to show edit form modal on studio page dependent of token.

--- src/components/Studio/Studio.tsx
+++ src/components/Studio/Studio.tsx
@@ -1,5 +1,8 @@
+import {useState} from "react";
 import {NavLink, useNavigate} from "react-router-dom";
+import {getToken} from "../../hooks/getToken";
 import {Button} from "../commons/Button/Button";
+import {EditStudio} from "../Admin/Studio/EditStudio/EditStudio";
 import {StudioType} from 'types';
 import {GameType} from 'types';
 
@@ -11,15 +14,19 @@ interface Props {
 }
 
 export const Studio = ({studio, games}: Props) => {
+    const [openEditForm, setOpenEditForm] = useState(false);
     const navigate = useNavigate();
+    const token = getToken();
     const {id, name, description, employees, founded, country} = studio;
 
     return (
         <div className="studio__container">
+            {openEditForm && <EditStudio/>}
             <Button
                 text="Wróc na poprzednią stronę"
                 onClick={() => navigate(-1)}
             />
+            {token && <Button text='Edytuj studio' onClick={() => setOpenEditForm(true)}/>}
             <img src={`${process.env.REACT_APP_API_URL}/studio/photo/${id}`} alt={`${name} logo`}/>
             <p>{description}</p>
             <div className='studio__details'>
