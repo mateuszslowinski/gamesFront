dd5a4cca85200ce7cd1453f7b9f362848c7df665
Mateusz <nowe7246@gmail.com>
2022-10-10 18:44:02 +0200

    Passed token and publisher data by another component instead api call.

--- src/components/Publisher/Publisher.tsx
+++ src/components/Publisher/Publisher.tsx
@@ -20,18 +20,23 @@ export const Publisher = ({studios, publisher}: Props) => {
     const handleGoToStudioSite = (id: string) => {
         navigate(`/studio/${id}`)
     };
-
+    const {name, description} = publisher
     return (
         <div className='publisher__container'>
-            {openEditForm === 1 && <EditPublisher closeModal={() => setOpenEditForm(0)}/>}
+            {openEditForm === 1 && <EditPublisher
+                token={token}
+                publisher={publisher}
+                closeModal={() => setOpenEditForm(0)}
+            />}
             {token && <Button
                 text='Edytuj wydawcę'
                 onClick={() => setOpenEditForm(1)}
             />
             }
-            <h2>{publisher.name}</h2>
-            <p>{publisher.description}</p>
-            <h3>Studia należące do {publisher.name}:</h3>
+            <h2>{name}</h2>
+            <span>Opis:<p>{description}</p></span>
+
+            <h3>Studia należące do {name}:</h3>
             <div className="studios__container">
                 {studios.map(studio => (
                     <div className="studio" key={studio.id} onClick={() => handleGoToStudioSite(studio.id)}>
