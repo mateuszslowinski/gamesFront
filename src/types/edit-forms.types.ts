a9359606eefcdcf7740f421a08fb7e8e0a9f2ed4
Mateusz <nowe7246@gmail.com>
2022-10-10 17:15:08 +0200

    Created edit publisher form type.

--- src/types/edit-forms.types.ts
+++ src/types/edit-forms.types.ts
@@ -1,3 +1,4 @@
-import {AddStudioFormType} from "./add-forms.types";
+import {AddPublisherFormType, AddStudioFormType} from "./add-forms.types";
 
 export type EditStudioFormType = AddStudioFormType;
+export type EditPublisherFormType = AddPublisherFormType;
