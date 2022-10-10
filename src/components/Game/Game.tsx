27ca650cbfbc069703e0c44f7c138768eb4b6789
Mateusz <nowe7246@gmail.com>
2022-10-10 11:30:20 +0200

    Added display game data on page.Added getting studio name from backend.

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,17 +1,40 @@
+import {useApi} from "../../hooks/useApi";
+import {Spinner} from "../commons/Spinner/Spinner";
+import {ErrorMessage} from "../commons/Messages/ErrorMessage/ErrorMessage";
+import {GameType} from 'types';
+import {StudioType} from 'types';
+
 import './Game.css';
 
-export const Game = () => {
+interface Props {
+    game: GameType;
+}
+
+export const Game = ({game}: Props) => {
+    const {id, name, description, releaseDate, developerId} = game;
+    const [studio, loadingStudio, errorStudio] = useApi<StudioType>({
+        method: 'get',
+        url: `/studio/${developerId}`
+    }, id)
+
+    if (!studio || errorStudio) return <ErrorMessage text={errorStudio}/>
     return (
         <div className='game__page__container'>
-            <h3>Nazwa gry</h3>
-            <img src="" alt="zdj"/>
-            <div className="game__details">
-                <span>Deweloper:<p>Nazwa studia</p></span>
-               <span>Data wydania: <p>Data  </p></span>
-            </div>
-            <div className='game__description'>
-                <p>Opis</p>
-            </div>
+            {loadingStudio
+                ? <Spinner/>
+                : (
+                    <>
+                        <h3>{name}</h3>
+                        <img src={`${process.env.REACT_APP_API_URL}/game/photo/${id}`} alt={`${name} logo`}/>
+                        <div className="game__details">
+                            <span>Deweloper:<p>{studio.name}</p></span>
+                            <span>Data wydania: <p>{releaseDate.toString().slice(0, 10)}</p></span>
+                        </div>
+                        <div className='game__description'>
+                            <p>{description}</p>
+                        </div>
+                    </>
+                )}
         </div>
     )
 }
