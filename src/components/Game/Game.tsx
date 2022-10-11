1187517ba942be1afb2412494384ec6e5f30e264
Mateusz <nowe7246@gmail.com>
2022-10-11 09:26:01 +0200

    Added button edit game form in game page depenets of token.

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,8 +1,11 @@
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {useApi} from "../../hooks/useApi";
+import {getToken} from "../../hooks/getToken";
 import {Spinner} from "../commons/Spinner/Spinner";
 import {ErrorMessage} from "../commons/Messages/ErrorMessage/ErrorMessage";
 import {Button} from "../commons/Button/Button";
+import {EditGame} from "../Admin/Game/EditGame/EditGame";
 import {GameType} from 'types';
 import {StudioType} from 'types';
 
@@ -13,8 +16,10 @@ interface Props {
 }
 
 export const Game = ({game}: Props) => {
-    const {id, name, description, releaseDate, developerId} = game;
     const navigate = useNavigate();
+    const token = getToken();
+    const [openEditForm, setOpenEditForm] = useState(0);
+    const {id, name, description, releaseDate, developerId} = game;
     const [studio, loadingStudio, errorStudio] = useApi<StudioType>({
         method: 'get',
         url: `/studio/${developerId}`
@@ -23,6 +28,7 @@ export const Game = ({game}: Props) => {
     if (!studio || errorStudio) return <ErrorMessage text={errorStudio}/>
     return (
         <div className='game__page__container'>
+            {openEditForm === 1 && <EditGame closeModal={() => setOpenEditForm(0)}  game={game}/>}
             {loadingStudio
                 ? <Spinner/>
                 : (
@@ -31,6 +37,10 @@ export const Game = ({game}: Props) => {
                             text="Wróc na poprzednią stronę"
                             onClick={() => navigate(-1)}
                         />
+                        {token && <Button
+                            text='Edytuj grę'
+                            onClick={() => setOpenEditForm(1)}
+                        />}
                         <h3>{name}</h3>
                         <img
                             src={`${process.env.REACT_APP_API_URL}/game/photo/${id}`}
