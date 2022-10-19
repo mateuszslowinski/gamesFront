3c46aafac727df04797e2e97c8a07a6396586d33
Mateusz <nowe7246@gmail.com>
2022-10-19 17:42:01 +0200

    Got platform data from backend.

--- src/pages/Platform/Platform.page.tsx
+++ src/pages/Platform/Platform.page.tsx
@@ -1,5 +1,19 @@
-export const PlatformPage = ()=>{
-    return(
-        <div>strona platformy</div>
+import {Platform} from "../../components/Platform/Platform";
+import {useApi} from "../../hooks/useApi";
+import {Spinner} from "../../components/commons/Spinner/Spinner";
+import {ErrorMessage} from "../../components/commons/Messages/ErrorMessage/ErrorMessage";
+import {PlatformType} from 'types';
+
+export const PlatformPage = () => {
+    const [platforms, platformsLoading, getPlatformsError] = useApi<PlatformType[]>({
+        method: 'get',
+        url: '/platform'
+    });
+
+    if (getPlatformsError) return <ErrorMessage text={getPlatformsError}/>
+    return (
+        <>
+            {platformsLoading || !platforms ? <Spinner/> : <Platform platforms={platforms}/>}
+        </>
     )
 }
