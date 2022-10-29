e2af0274f18766750fc0abe629181d0e7f955b76
Mateusz <nowe7246@gmail.com>
2022-10-29 16:12:50 +0200

    Created searchbar component with basic structure.

---
+++ src/components/commons/Searchbar/Searchbar.tsx
@@ -0,0 +1,10 @@
+export const Searchbar = () => {
+    return (
+        <div className="searchbar_container">
+            <input
+                type="text"
+                placeholder="Wyszukaj grę..."/>
+            <button className='btn'>Szukaj</button>
+        </div>
+    )
+}
