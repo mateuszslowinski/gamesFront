a9ed9374f5ae5ee08537de22fd593cb5fdad6b98
Mateusz <nowe7246@gmail.com>
2022-10-29 16:46:48 +0200

    Added searchbar logic.

--- src/components/commons/Searchbar/Searchbar.tsx
+++ src/components/commons/Searchbar/Searchbar.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Searchbar.css';
 
 export const Searchbar = () => {
+    const [term, setTerm] = useState<string>('');
+    const navigate = useNavigate();
+
+    const search = () => {
+        navigate(`/search/${term}`)
+    }
     return (
         <div className="searchbar_container">
             <input
                 type="text"
-                placeholder="Wyszukaj grę..."/>
-            <button className='btn'>Szukaj</button>
+                placeholder="Wyszukaj grę..."
+                onChange={(e) => setTerm(e.target.value)}
+                onKeyPress={e => {
+                    if (e.key === 'Enter') search()
+                }}
+            />
+            <button className='btn' onClick={search}>
+                Szukaj
+            </button>
         </div>
     )
 }
