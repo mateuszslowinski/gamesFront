e4c603f12916675c7b6e5cb5c1ca3482b5defc25
Mateusz <nowe7246@gmail.com>
2022-10-29 10:22:39 +0200

    Added animated routes component to app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
 import {Header} from './components/commons/Header/Header';
-import {HomePage} from "./pages/Home/Home.page";
-import {PublisherPage} from "./pages/Publisher/Publisher.page";
-import {StudioPage} from "./pages/Studio/Studio.page";
-import {Login} from "./components/Login/Login";
-import {ProtectedRoutes} from "./utils/ProtectedRoutes";
-import {AdminPage} from "./pages/AdminPage/AdminPage";
-import {GamePage} from "./pages/Game/Game.page";
-import {PlatformPage} from "./pages/Platform/Platform.page";
+import {AnimatedRoutes} from "./components/AnimatedRoutes/AnimatedRoutes";
 
-export const App = () => {
-    return (
-        <>
-            <Header/>
-            <Routes>
-                <Route element={<ProtectedRoutes/>}>
-                    <Route path='/admin' element={<AdminPage/>}/>
-                </Route>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='/auth/login' element={<Login/>}/>
-                <Route path='/publisher/:name' element={<PublisherPage/>}/>
-                <Route path='/studio/:name' element={<StudioPage/>}/>
-                <Route path='/game/:name' element={<GamePage/>}/>
-                <Route path='/platform' element={<PlatformPage/>}/>
-            </Routes>
-        </>
-    );
-}
+export const App = () => (
+    <>
+        <Header/>
+        <AnimatedRoutes/>
+    </>
+);
+
 
