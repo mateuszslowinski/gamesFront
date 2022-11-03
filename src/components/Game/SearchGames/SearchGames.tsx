17e1417d38e65cbd18fdec8d80d7220337aa3060
Mateusz <nowe7246@gmail.com>
2022-11-03 10:45:59 +0100

    Getting games from backend dependents of search term.

--- src/components/Game/SearchGames/SearchGames.tsx
+++ src/components/Game/SearchGames/SearchGames.tsx
@@ -1,9 +1,22 @@
-import {useParams, useSearchParams} from "react-router-dom";
+import {useSearchParams} from "react-router-dom";
+import {useApi} from "../../../hooks/useApi";
+import {GameType} from 'types'
+import {Spinner} from "../../commons/Spinner/Spinner";
 
-export const SearchGames =()=>{
-    const[searchParams,setSearchParams]=useSearchParams()
-    console.log(searchParams.get('search'))
-    return(
-        <div>searchgames</div>
+export const SearchGames = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const term = searchParams.get('search')
+    const [games, loadingGames, errorGames] = useApi<GameType[]>({
+        method: 'get',
+        url: `/game/search/${term}`
+    }, [term]);
+
+    return (
+        <div className='page__container'>
+            {loadingGames
+                ? <Spinner/>
+                : games?.length !== 0 ? <h2>wyniki</h2> : <h2>brak wynikow wyszukiwania</h2>
+            }
+        </div>
     )
 }
