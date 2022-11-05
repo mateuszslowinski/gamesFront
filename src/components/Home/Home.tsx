07c06f0d3f8815fed4d7ca7d2c1528279427fa51
Mateusz <nowe7246@gmail.com>
2022-11-05 10:44:02 +0100

    Created home component and css styles.

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+
+
+import './Home.css';
+
+
+export const Home = () => {
+
+
+    return (
+        <div className='home__container'>
+            <h2>Witaj na games world</h2>
+            <p>opis tego co się tu znajduje</p>
+
+        </div>
+
+    )
+}
