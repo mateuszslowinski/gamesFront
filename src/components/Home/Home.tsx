fd000c82888d9c2ab8982756b40f56691d954de0
Mateusz <nowe7246@gmail.com>
2022-11-05 11:24:12 +0100

    Created simple structure of home page.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,41 @@
-
+import {PublisherType} from 'types';
 
 import './Home.css';
+import {NavLink} from "react-router-dom";
+import {PremiereGameCard} from "../Game/PremiereGameCard/PremiereGameCard";
 
+interface Props {
+    publishers: PublisherType[]
+}
 
-export const Home = () => {
+export const Home = ({publishers}: Props) => {
 
 
     return (
         <div className='home__container'>
-            <h2>Witaj na games world</h2>
-            <p>opis tego co się tu znajduje</p>
-
+            <div>
+                <h2>Witaj na games world</h2>
+                <p>opis tego co się tu znajduje</p>
+            </div>
+            <div>
+                <h4>Wydawcy:</h4>
+                {publishers.map(publisher => (
+                    <div className='publisher__links__container'>
+                        <NavLink
+                            key={publisher.id}
+                            to={`/publisher/${(publisher.name).toLowerCase()}`}
+                        >
+                            {publisher.name}
+                        </NavLink>
+                    </div>
+                ))}
+            </div>
+            <div>
+                losowe studia
+            </div>
+            <div>
+                <PremiereGameCard/>
+            </div>
         </div>
 
     )
