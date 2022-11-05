69ef574f4201507c658b748c2f7b67021a98c26a
Mateusz <nowe7246@gmail.com>
2022-11-05 11:24:57 +0100

    Getting publisher data from backend and pass to home component.

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,3 +1,19 @@
-export const HomePage = ()=>(
-    <div>strona główna</div>
-)
+import {Home} from "../../components/Home/Home";
+import {useApi} from "../../hooks/useApi";
+import {Spinner} from "../../components/commons/Spinner/Spinner";
+import {ErrorMessage} from "../../components/commons/Messages/ErrorMessage/ErrorMessage";
+import {PublisherType} from 'types';
+
+export const HomePage = () => {
+    const [publishers, publisherLoading, publisherError] = useApi<PublisherType[]>({
+        method: 'get',
+        url: '/publisher'
+    });
+
+    if (publisherError) return <ErrorMessage text={publisherError}/>
+    return (
+        <div className='page__container'>
+            {(publisherLoading || !publishers) ? <Spinner/> : <Home publishers={publishers}/>}
+        </div>
+    )
+}
