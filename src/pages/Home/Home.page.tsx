d295c777631ce2020c5901107a61c413db3aac93
Mateusz <nowe7246@gmail.com>
2022-11-05 14:58:40 +0100

    Created random number funtion.

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -3,17 +3,26 @@ import {useApi} from "../../hooks/useApi";
 import {Spinner} from "../../components/commons/Spinner/Spinner";
 import {ErrorMessage} from "../../components/commons/Messages/ErrorMessage/ErrorMessage";
 import {PublisherType} from 'types';
+import {StudioType} from 'types';
+
 
 export const HomePage = () => {
     const [publishers, publisherLoading, publisherError] = useApi<PublisherType[]>({
         method: 'get',
         url: '/publisher'
     });
+    const [studios, loadingStudios, errorStudios] = useApi<StudioType[]>({
+        method: 'get',
+        url: `/studio/`
+    });
 
     if (publisherError) return <ErrorMessage text={publisherError}/>
+    if (errorStudios) return <ErrorMessage text={errorStudios}/>
     return (
         <div className='page__container'>
-            {(publisherLoading || !publishers) ? <Spinner/> : <Home publishers={publishers}/>}
+            {(publisherLoading || !publishers || loadingStudios || !studios)
+                ? <Spinner/>
+                : <Home publishers={publishers} studios={studios}/>}
         </div>
     )
 }
